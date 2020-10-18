bada5a2f27cfded2936b28bd22394d7a0279fa47
Enzo Lizama <lizama.enzo@gmail.com>
2020-10-18 03:02:14 -0500

    add utils to warp inside dashboard

---
+++ src/utils/dashboard-wrapper.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PlatformLayout from '@/components/dashboard';
+
+const dashboardWrapper = (Component: React.FC): React.FC => {
+  const Wrapper: React.FC = () => (
+    <PlatformLayout>
+      <Component />
+    </PlatformLayout>
+  );
+  return Wrapper;
+};
+
+export default dashboardWrapper;
