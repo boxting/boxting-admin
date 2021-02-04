81be0bb18ad823144deabc17317591e16bf2e2ff
RodrigoAGM <rod-gm@hotmail.com>
2021-02-04 02:32:30 -0500

    Update error mapper with login errors

--- src/data/error/error.mapper.ts
+++ src/data/error/error.mapper.ts
@@ -1,9 +1,12 @@
 export const ErrorMapper = {
-    '500'   :   'Ocurrió un error en el sistema',
-    '4004'  :   'La fecha y hora de inicio no pueden ser anteriores a la fecha y hora actuales',
-    '4005'  :   'La fecha y hora de fin debe ser posterior a la fecha y hora de inicio',
-    '5001'  :   'No pueden existir dos códigos de acceso iguales',
-    '5002'  :   'No pueden existir dos códigos de acceso iguales',
+    '500'   :   'Ocurrió un error en el sistema.',
+    '1001'  :   'Usuario o contraseña incorrectos.',
+    '1002'  :   'Usuario o contraseña incorrectos.',
+    '1003'  :   'Usuario o contraseña incorrectos.',
+    '4004'  :   'La fecha y hora de inicio no pueden ser anteriores a la fecha y hora actuales.',
+    '4005'  :   'La fecha y hora de fin debe ser posterior a la fecha y hora de inicio.',
+    '5001'  :   'No pueden existir dos códigos de acceso iguales.',
+    '5002'  :   'No pueden existir dos códigos de acceso iguales.',
     '5003'  :   '123',
     '5004'  :   '123',
 }
