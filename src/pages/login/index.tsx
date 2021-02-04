7f55c46a8ac27f4439653bb2c3b423e049a2ddbb
RodrigoAGM <rod-gm@hotmail.com>
2021-02-04 02:34:24 -0500

    Use cookies manager to save tokens

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,15 @@
 import { NextPage } from 'next';
 import React from 'react';
 import Cookies from 'js-cookie';
-// eslint-disable-next-line object-curly-newline
 import { Flex } from '@chakra-ui/core';
 
 import LoginForm from './login_form';
+import CookiesManager from '@/data/utils/cookies.manager';
 
 const onUserLogin = (authToken): void => {
-  Cookies.set(`token`, authToken);
-};
+  CookiesManager.getInstance()._setToken(authToken, '')
+}
+
 const LoginPage: NextPage = () => (
   <Flex align="center" justify="center" minH="100vh">
     <LoginForm onSignIn={onUserLogin} />
