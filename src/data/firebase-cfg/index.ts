e5023de71dd94590777ed3cae7f796c936dec2d3
RodrigoAGM <rod-gm@hotmail.com>
2021-02-12 21:05:35 -0500

    Create firebase config and init

---
+++ src/data/firebase-cfg/index.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+import 'firebase/storage'
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+    projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+    storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
+    messagingSenderId: process.env.FIREBASE_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const storage = firebase.storage()
+
+export { storage, firebase }
