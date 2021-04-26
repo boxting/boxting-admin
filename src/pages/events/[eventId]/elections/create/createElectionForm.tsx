04002bc0e80f734ca8d219ce0468f728765ea2ef
juanCR44 <56515361+juanCR44@users.noreply.github.com>
2021-04-25 19:06:32 -0500

    Update createElectionForm.tsx

--- src/pages/events/[eventId]/elections/create/createElectionForm.tsx
+++ src/pages/events/[eventId]/elections/create/createElectionForm.tsx
@@ -37,9 +37,7 @@ const ElectionCreateForm = (props: ElectionCreateFormProps) => {
 	const [winners, setWinners] = useState<number>(0)
 
 	const [errorNameLength, setErrorNameLength] = useState(false)
-	const [errorNameregEx, setErrorNameregEx] = useState(false)
 	const [errorInformationLength, setErrorInformationLength] = useState(false)
-	const [errorInformationregEx, setErrorInformationregEx] = useState(false)
 
 	// Constants
 	const MIN_LENGTH_NAME = 5;
@@ -143,31 +141,12 @@ const ElectionCreateForm = (props: ElectionCreateFormProps) => {
 		}
 	}
 
-	function verififyRegex(value){
-		let regEx
-		let val = []
-		for(let x of value){
-			regEx = x.charCodeAt(0)
-			if ((regEx > 122 || (regEx < 97 && regEx > 90) || regEx < 65) && regEx != 32){
-				val.push(x)
-			}
-		}
-		return val.length >= 1
-	}
-
 	function verifyInputName(){
 		if ((name.length < MIN_LENGTH_NAME || name.length > MAX_LENGTH_NAME) && name.length != 0){
 			setErrorNameLength(true)
 		}
 		else{
 			setErrorNameLength(false)
-		}
-
-		if (verififyRegex(name)){
-			setErrorNameregEx(true)
-		}
-		else{
-			setErrorNameregEx(false)
 		}
 	}
 
@@ -178,22 +157,11 @@ const ElectionCreateForm = (props: ElectionCreateFormProps) => {
 		else{
 			setErrorInformationLength(false)
 		}
-
-		if (verififyRegex(information)){
-			setErrorInformationregEx(true)
-		}
-		else{
-			setErrorInformationregEx(false)
-		}
 	}
 
 	function errorMessageName(){
 		if (errorNameLength){
 			return "Nombre incorrecto, no debe ser menor a 5 y mayor a 100 caracteres."
-		}
-
-		if (errorNameregEx){
-			return "Nombre incorrecto, no debe contener caracteres especiales."
 		}
 	}
 
@@ -202,15 +170,11 @@ const ElectionCreateForm = (props: ElectionCreateFormProps) => {
 		if (errorInformationLength){
 			return "Información incorrecta, no debe ser menor a 10 y mayor a 500 caracteres."
 		}
-
-		if (errorInformationregEx){
-			return "Información incorrecta, no debe contener caracteres especiales."
-		}
 	}
 
 	return (
 		<Box>
-			<FormControl isInvalid={errorNameregEx || errorNameLength}>
+			<FormControl isInvalid={errorNameLength}>
 				<FormLabel>Nombre</FormLabel>
 				<Input
 					value={name}
@@ -220,7 +184,7 @@ const ElectionCreateForm = (props: ElectionCreateFormProps) => {
 				/>
 				<FormErrorMessage>{errorMessageName()}</FormErrorMessage>
 			</FormControl>
-			<FormControl mt={4} isInvalid={errorInformationregEx || errorInformationLength}>
+			<FormControl mt={4} isInvalid={errorInformationLength}>
 				<FormLabel>Descripción</FormLabel>
 				<Textarea
 					value={information}
@@ -255,7 +219,7 @@ const ElectionCreateForm = (props: ElectionCreateFormProps) => {
 			</FormControl>
 			<FormControl mt={4}>
 				<BoxtingButton
-					isDisabled = {errorNameregEx || errorNameLength || errorInformationregEx || errorInformationLength}
+					isDisabled = {errorNameLength || errorInformationLength}
 					isLoading={appState.loading}
 					typeBtn={ButtonType.primary}
 					text="Guardar"
