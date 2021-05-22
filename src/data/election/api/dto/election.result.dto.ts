1b678ecc7ccb4353d7603a06e1679cb10af70262
RodrigoAGM <rod-gm@hotmail.com>
2021-05-22 16:51:47 -0500

    Update result dto

--- src/data/election/api/dto/election.result.dto.ts
+++ src/data/election/api/dto/election.result.dto.ts
@@ -1,10 +1,19 @@
 import { CandidateDto } from "@/data/candidate/api/dto/candidate.dto";
 
+type CandidateResult = {
+    id: string,
+    firstName: string,
+    lastName: string,
+    imageUrl?: string,
+    electionId: string,
+    voteCount?: number
+}
+
 export interface ElectionResultDto {
     election: {
         id: string,
         name: string
     },
     totalVotes: number,
-    candidates: CandidateDto[]
+    candidates: CandidateResult[]
 }
